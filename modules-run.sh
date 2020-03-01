a86f2c54b68926ee160cf70f9d0d9630379317f0
peepvl <peepvl@126.com>
2020-03-01 16:33:00 +0800

    测试 自动构建、打包、并生成docker images

--- modules-run.sh
+++ modules-run.sh
@@ -2,5 +2,5 @@
 
 echo  "构建整个Modules项目，并运行Docker 容器 ... ... "
 
-sudo usermod -a -G docker jenkins
-sudo docker build -f ./base/Dockerfile -t modules-base:v1.0 .
+usermod -a -G docker jenkins
+docker build -f ./base/Dockerfile -t modules-base:v1.0 .
