b553be879af19144ff183d5eb6f0ea097a2e3640
peepvl <peepvl@126.com>
2020-03-03 17:23:58 +0800

    测试 自动构建、打包、并生成docker images

--- modules-run.sh
+++ modules-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo  "构建整个Modules项目，并运行Docker 容器 ... ... "
+echo  "构建整个Modules项目，//并运行Docker 容器 ... ... "
 
 sh ./base/base-build.sh
 # docker build -f ./base/Dockerfile -t modules-base:v1.0 .
